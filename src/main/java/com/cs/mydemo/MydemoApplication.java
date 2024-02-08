f69ff9d1f3049c6dfc2dee88247043b1dbf88190
ajit0507 <ajitshivajipatil@gmail.com>
2024-02-08 18:08:29 +0530

    code change for rest point

--- src/main/java/com/cs/mydemo/MydemoApplication.java
+++ src/main/java/com/cs/mydemo/MydemoApplication.java
@@ -2,12 +2,20 @@ package com.cs.mydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MydemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MydemoApplication.class, args);
     }
 
+    @GetMapping("/greet")
+    String greet()
+    {
+    	return "hello";
+    }
 }
